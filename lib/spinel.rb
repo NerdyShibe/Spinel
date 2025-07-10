8f605fcdac6851c63934c7ebc564f6a358e6b7eb
Fernando Eufrásio <fernando.epm1@gmail.com>
2025-07-10 11:35:45 -0300

    feat: implement the 16-bit registers and flags

--- lib/spinel.rb
+++ lib/spinel.rb
@@ -4,42 +4,50 @@ require 'debug'
 
 require_relative 'spinel/emulator'
 require_relative 'spinel/constants'
+
+require_relative 'spinel/data/instructions'
+
+require_relative 'spinel/hardware/bus'
 require_relative 'spinel/hardware/cpu'
 require_relative 'spinel/hardware/ppu'
+require_relative 'spinel/hardware/registers'
+require_relative 'spinel/hardware/timer'
+
 require_relative 'spinel/cartridge/rom'
 
 #
 # TODO: Write detailed comment about the Spinel module
 module Spinel
   VERSION = '0.0.1'
-  TEST = 1
 
-  # Get the ROM file from command line argument
-  file_path = ARGV[0]
+  unless defined?(RSpec)
+    # Get the ROM file from command line argument
+    file_path = ARGV[0]
 
-  # Verify if the argument was provided
-  if file_path.nil?
-    puts 'Error: You must provide a path to a GB rom'
-    puts 'Usage: $ ruby lib/spinel.rb /path/to/rom.gb'
+    # Verify if the argument was provided
+    if file_path.nil?
+      puts 'Error: You must provide a path to a GB rom'
+      puts 'Usage: $ ruby lib/spinel.rb /path/to/rom.gb'
 
-    exit 1
-  end
+      exit 1
+    end
 
-  # Do not allow directories as arguments
-  if File.directory?(file_path)
-    puts 'Error: Path provided is a directory'
-    puts 'Usage: $ ruby lib/spinel.rb /path/to/rom.gb'
+    # Do not allow directories as arguments
+    if File.directory?(file_path)
+      puts 'Error: Path provided is a directory'
+      puts 'Usage: $ ruby lib/spinel.rb /path/to/rom.gb'
 
-    exit 2
-  end
+      exit 2
+    end
 
-  begin
-    # Creates the initial instance of the Emulator
-    # to start the emulation process
-    Spinel::Emulator.new(file_path)
-  rescue Errno::ENOENT, file_path
-    puts "Error: The file at #{file_path} does not exist"
+    begin
+      # Creates the initial instance of the Emulator
+      # to start the emulation process
+      Spinel::Emulator.new(file_path)
+    rescue Errno::ENOENT, file_path
+      puts "Error: The file at #{file_path} does not exist"
 
-    exit 3
+      exit 3
+    end
   end
 end
--- lib/spinel/hardware/registers.rb
+++ lib/spinel/hardware/registers.rb
@@ -8,7 +8,6 @@ module Spinel
     # F => Flags register
     class Registers
       attr_accessor :a, :f, :b, :c, :d, :e, :h, :l, :sp, :pc
-      attr_writer :zf, :sf, :hcf, :cf, :af, :bc, :de, :hl
 
       def initialize
         @a = 0x00
@@ -21,53 +20,117 @@ module Spinel
         @l = 0x00
 
         @sp = 0x0000
-        @pc = 0x0000
+        @pc = 0x0100
       end
 
+      # CPU uses a combination of the 8-bit registers
+      # to create 16-bit registers
       #
-      # Zero Flag
-      # Checks if the Bit7 of the Flags register is set
-      def zf
-        (@f & 0b10000000) != 0
-      end
-
-      #
-      # Subtraction Flag
-      # Checks if the Bit6 of the Flags register is set
-      def sf
-        (@f & 0b01000000) != 0
-      end
-
-      #
-      # Half Carry Flag
-      # Checks if the Bit5 of the Flags register is set
-      def hcf
-        (@f & 0b00100000) != 0
-      end
-
-      #
-      # Carry Flag
-      # Checks if the Bit4 of the Flags register is set
-      def cf
-        (@f & 0b00010000) != 0
-      end
-
-      #
-      # Accumulator + Flags registers
       def af
-        @af = (@a << 8) + @f
+        @af = (@a << 8) | @f
+      end
+
+      def af=(value)
+        @a = value >> 8
+        @f = (value & 0xFF) & 0xF0 # Lower 4 bits are always 0
       end
 
       def bc
-        @bc = (@b << 8) + @c
+        @bc = (@b << 8) | @c
+      end
+
+      def bc=(value)
+        @b = value >> 8
+        @c = value & 0xFF
       end
 
       def de
-        @de = (@d << 8) + @e
+        @de = (@d << 8) | @e
+      end
+
+      def de=(value)
+        @d = value >> 8
+        @e = value & 0xFF
       end
 
       def hl
-        @hl = (@h << 8) + @l
+        @hl = (@h << 8) | @l
+      end
+
+      def hl=(value)
+        @h = value >> 8
+        @l = value & 0xFF
+      end
+
+      # Zero Flag
+      # Bit7 of the Flags register (@f)
+      # Determines if the result of the last
+      # arithmetic operation was zero
+      #
+      def z_flag
+        (@f & 0b10000000) >> 7
+      end
+
+      def z_flag=(bit_value)
+        if (bit_value & 1) == 1
+          @f |= 0b10000000
+        else
+          @f &= 0b01111111
+        end
+      end
+
+      # Subtraction Flag
+      # Bit6 of the Flags register (@f)
+      # Determines if the last operation
+      # was a subtraction
+      #
+      def n_flag
+        (@f & 0b01000000) >> 6
+      end
+
+      def n_flag=(bit_value)
+        if (bit_value & 1) == 1
+          @f |= 0b01000000
+        else
+          @f &= 0b10111111
+        end
+      end
+
+      # Half Carry Flag
+      # Bit5 of the Flags register (@f)
+      # ??
+      #
+      def h_flag
+        (@f & 0b00100000) >> 5
+      end
+
+      def h_flag=(bit_value)
+        if (bit_value & 1) == 1
+          @f |= 0b00100000
+        else
+          @f &= 0b11011111
+        end
+      end
+
+      # Carry Flag
+      # Bit4 of the Flags register (@f)
+      #
+      # Value is set to 1 on the following scenarios:
+      # When the result of an 8-bit addition is higher than $FF.
+      # When the result of a 16-bit addition is higher than $FFFF.
+      # When the result of a subtraction or comparison is lower than zero.
+      # When a rotate/shift operation shifts out a “1” bit.
+      #
+      def c_flag
+        (@f & 0b00010000) >> 4
+      end
+
+      def c_flag=(bit_value)
+        if (bit_value & 1) == 1
+          @f |= 0b00010000
+        else
+          @f &= 0b11101111
+        end
       end
     end
   end
