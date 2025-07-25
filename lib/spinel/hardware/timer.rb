57332e82b1aa3e0631e7a0b4c988218185943356
Fernando Eufrásio <fernando.epm1@gmail.com>
2025-07-25 19:55:36 -0300

    feat: implement the timer and its registers

--- lib/spinel/hardware/timer.rb
+++ lib/spinel/hardware/timer.rb
@@ -4,7 +4,90 @@ module Spinel
   module Hardware
     # This will describe the Emulator Timer
     class Timer
-      def tick; end
+      def initialize(interrupts)
+        @interrupts = interrupts
+
+        @registers = Registers.new
+      end
+
+      def step(cycles)
+        cycles.times { tick }
+      end
+
+      def read_byte(address)
+        case address
+        when 0xFF04 then (@registers.div >> 8)
+        when 0xFF05 then @registers.tima
+        when 0xFF06 then @registers.tma
+        when 0xFF07 then @registers.tac
+        end
+      end
+
+      def write_byte(address, value)
+        case address
+        when 0xFF04
+          old_trigger_bit = @registers.div_bit_trigger
+          @registers.div = 0x00
+
+          if timer_enabled? && old_trigger_bit == 1
+            increment_tima
+          end
+        when 0xFF05 then @registers.tima = value
+        when 0xFF06 then @registers.tma = value
+        when 0xFF07
+          old_trigger_bit = @registers.div_bit_trigger
+          was_enabled = timer_enabled?
+
+          @registers.tac = value
+
+          new_trigger_bit = @registers.div_bit_trigger
+          is_now_disabled = !timer_enabled?
+
+          if timer_enabled? && old_trigger_bit == 1 && new_trigger_bit == 0
+            increment_tima
+          end
+
+          if was_enabled && is_now_disabled
+            if old_trigger_bit == 1
+              increment_tima
+            end
+          end
+        end
+      end
+
+      private
+
+      def timer_enabled?
+        @registers.tac_enable == 1
+      end
+
+      def update_tima?(old_bit_trigger)
+        return false unless timer_enabled?
+
+        new_bit_trigger = @registers.div_trigger_bit
+
+        # Check for a falling edge 1 => 0 in the specified bit
+        old_bit_trigger.allbits?(1) && new_bit_trigger.nobits?(0)
+      end
+
+      def request_interrupt
+        @interrupts.request(:timer)
+      end
+
+      def tick
+        old_bit_trigger = @registers.div_bit_trigger
+        @registers.div += 1
+
+        return unless update_tima?(old_bit_trigger)
+
+        @registers.tima += 1
+
+        # Check for TIMA overflow from 0xFF to 0x00
+        return unless @registers.tima.zero?
+
+        @registers.tima = @registers.tma
+        request_interrupt
+      end
     end
   end
 end
