c119524fd7ae55da5abe48f18a420ba9e7be9a07
Fernando Eufrásio <fernando.epm1@gmail.com>
2025-07-26 14:34:50 -0300

    refactor: dry some methods in the cpu registers

--- lib/spinel/hardware/cpu/registers.rb
+++ lib/spinel/hardware/cpu/registers.rb
@@ -6,21 +6,24 @@ module Spinel
     class Cpu
       # Will hold the state of the CPU registers
       #
+      # 8-bit registers => A, F, B, C, D, E, H, L
+      # 16-bit registers => SP, PC
+      # 16-bit register pairs => AF, BC, DE, HL
+      #
       class Registers
-        attr_accessor :pc
-        attr_reader :a, :f, :b, :c, :d, :e, :h, :l, :sp
+        attr_reader :a, :f, :b, :c, :d, :e, :h, :l, :sp, :pc
 
         def initialize
-          @a = 0x00
-          @f = 0x00
+          @a = 0x01
+          @f = 0xB0
           @b = 0x00
-          @c = 0x00
+          @c = 0x13
           @d = 0x00
-          @e = 0x00
-          @h = 0x00
-          @l = 0x00
+          @e = 0xD8
+          @h = 0x01
+          @l = 0x4D
 
-          @sp = 0x0000
+          @sp = 0xFFFE
           @pc = 0x0100
         end
 
@@ -52,46 +55,39 @@ module Spinel
           @l = value & 0xFF
         end
 
-        # CPU uses a combination of the 8-bit registers
-        # to create 16-bit registers
-        #
         def af
-          @af = (@a << 8) | @f
+          (@a << 8) | @f
         end
 
         def af=(value)
-          value &= 0xFFFF
-          @a = value >> 8
+          @a = (value >> 8) & 0xFF
           @f = value & 0xF0 # Lower 4 bits are always 0
         end
 
         def bc
-          @bc = (@b << 8) | @c
+          (@b << 8) | @c
         end
 
         def bc=(value)
-          value &= 0xFFFF
-          @b = value >> 8
+          @b = (value >> 8) & 0xFF
           @c = value & 0xFF
         end
 
         def de
-          @de = (@d << 8) | @e
+          (@d << 8) | @e
         end
 
         def de=(value)
-          value &= 0xFFFF
-          @d = value >> 8
+          @d = (value >> 8) & 0xFF
           @e = value & 0xFF
         end
 
         def hl
-          @hl = (@h << 8) | @l
+          (@h << 8) | @l
         end
 
         def hl=(value)
-          value &= 0xFFFF
-          @h = value >> 8
+          @h = (value >> 8) & 0xFF
           @l = value & 0xFF
         end
 
@@ -99,49 +95,41 @@ module Spinel
           @sp = value & 0xFFFF
         end
 
+        def pc=(value)
+          @pc = value & 0xFFFF
+        end
+
         # Zero Flag => Bit7 of the Flags register (@f)
         # Needs to be set if the result of the last operation was zero
         #
         def z_flag
-          (@f & 0b10000000) >> 7
+          @f[7]
         end
 
-        def z_flag=(set)
-          if set
-            @f |= 0b10000000
-          else
-            @f &= 0b01111111
-          end
+        def z_flag=(value)
+          set_bit(7, value)
         end
 
         # Subtraction Flag => Bit6 of the Flags register (@f)
         # Needs to be set if the last operation was a subtraction
         #
         def n_flag
-          (@f & 0b01000000) >> 6
+          @f[6]
         end
 
-        def n_flag=(set)
-          if set
-            @f |= 0b01000000
-          else
-            @f &= 0b10111111
-          end
+        def n_flag=(value)
+          set_bit(6, value)
         end
 
         # Half Carry Flag => Bit5 of the Flags register (@f)
         # Needs to be set if the last operation resulted in a half carry
         #
         def h_flag
-          (@f & 0b00100000) >> 5
+          @f[5]
         end
 
-        def h_flag=(set)
-          if set
-            @f |= 0b00100000
-          else
-            @f &= 0b11011111
-          end
+        def h_flag=(value)
+          set_bit(5, value)
         end
 
         # Carry Flag => Bit4 of the Flags register (@f)
@@ -153,15 +141,11 @@ module Spinel
         # When a rotate/shift operation shifts out a “1” bit.
         #
         def c_flag
-          (@f & 0b00010000) >> 4
+          @f[4]
         end
 
-        def c_flag=(set)
-          if set
-            @f |= 0b00010000
-          else
-            @f &= 0b11101111
-          end
+        def c_flag=(value)
+          set_bit(4, value)
         end
 
         def update_flags(z:, n:, h:, c:) # rubocop:disable Naming/MethodParameterName
@@ -169,6 +153,18 @@ module Spinel
           self.n_flag = n unless n.nil?
           self.h_flag = h unless h.nil?
           self.c_flag = c unless c.nil?
+        end
+
+        private
+
+        def set_bit(position, value)
+          mask = 1 << position
+
+          if value
+            @f |= mask
+          else
+            @f &= ~mask
+          end
         end
       end
     end
