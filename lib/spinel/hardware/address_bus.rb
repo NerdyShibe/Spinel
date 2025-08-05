248dec6c0ee8f5b8ef347b1d667fc86150f2258f
Fernando Eufrásio <fernando.epm1@gmail.com>
2025-08-04 21:30:25 -0300

    refactor: remove the use of Ranges in the address bus

--- lib/spinel/hardware/address_bus.rb
+++ lib/spinel/hardware/address_bus.rb
@@ -6,17 +6,6 @@ module Spinel
     # Which is used to map the address values for the ROM, RAM, and I/O.
     #
     class AddressBus
-      IN_ROM_BANK_0_RANGE   = (0x0000..0x3FFF)
-      IN_ROM_BANK_N_RANGE   = (0x4000..0x7FFF)
-      IN_VIDEO_RAM_RANGE    = (0x8000..0x9FFF)
-      IN_EXTERNAL_RAM_RANGE = (0xA000..0xBFFF)
-      IN_WORK_RAM_RANGE     = (0xC000..0xDFFF)
-      IN_ECHO_RAM_RANGE     = (0xE000..0xFDFF)
-      IN_OAM_RANGE          = (0xFE00..0xFE9F)
-      IN_NOT_USABLE_RANGE   = (0xFEA0..0xFEFF)
-      IN_IO_REGISTERS_RANGE = (0xFF00..0xFF7F)
-      IN_HIGH_RAM_RANGE     = (0xFF80..0xFFFE)
-
       def initialize( # rubocop:disable Metrics/ParameterLists
         cartridge,
         ppu,
@@ -46,29 +35,40 @@ module Spinel
       # @return [Integer] => 8-bit value
       #
       def read_byte(address)
-        case address
-        when IN_ROM_BANK_0_RANGE   then @cartridge.rom_read(address)
-        when IN_ROM_BANK_N_RANGE   then @cartridge.rom_read(address)
-        when IN_VIDEO_RAM_RANGE    then @vram.read_byte(address)
-        when IN_EXTERNAL_RAM_RANGE then @cartridge.ram_read(address)
-        when IN_WORK_RAM_RANGE     then @wram.read_byte(address)
-        when IN_ECHO_RAM_RANGE     then @wram.read_byte(address - 0x2000)
-        when IN_OAM_RANGE          then @ppu.read_oam(address)
-        when IN_NOT_USABLE_RANGE   then 0xFF
-        when IN_IO_REGISTERS_RANGE
-          case address
-          when 0xFF00         then @joypad.p1
-          when 0xFF01         then @serial_port.sb
-          when 0xFF02         then @serial_port.sc
-          when 0xFF04..0xFF07 then @timer.read_registers(address)
-          when 0xFF0F         then @interrupts.if
-          when 0xFF40..0xFF4B then @ppu.read_registers(address)
-          else 0xFF
+        if address.between?(0x0000, 0x7FFF)
+          @cartridge.rom_read(address)
+        elsif address <= 0x9FFF
+          @vram.read_byte(address)
+        elsif address <= 0xBFFF
+          @cartridge.ram_read(address)
+        elsif address <= 0xDFFF
+          @wram.read_byte(address)
+        elsif address <= 0xFDFF
+          @wram.read_byte(address - 0x2000)
+        elsif address <= 0xFE9F
+          @ppu.read_oam(address)
+        elsif address <= 0xFEFF
+          0xFF
+        elsif address <= 0xFF7F
+          if address == 0xFF00
+            @joypad.p1
+          elsif address == 0xFF01
+            @serial_port.sb
+          elsif address == 0xFF02
+            @serial_port.sc
+          elsif address <= 0xFF07
+            @timer.read_registers(address)
+          elsif address == 0xFF0F
+            @interrupts.if
+          else
+            0xFF
           end
-        when IN_HIGH_RAM_RANGE then @hram.read_byte(address)
-        when 0xFFFF then @interrupts.ie
+        elsif address <= 0xFFFE
+          @hram.read_byte(address)
+        elsif address == 0xFFFF
+          @interrupts.ie
         else
-          raise StandardError, "Memory out of bounds at: #{format('%04X', address)}"
+          raise StandardError, "#{address} is out of bounds"
         end
       end
 
@@ -79,29 +79,38 @@ module Spinel
       # @param value [Integer] => 8-bit value
       #
       def write_byte(address, value)
-        case address
-        when 0x0000..0x3FFF then @cartridge.rom_write(address, value)
-        when 0x4000..0x7FFF then @cartridge.rom_write(address, value)
-        when 0x8000..0x9FFF then @vram.write_byte(address, value)
-        when 0xA000..0xBFFF then @cartridge.ram_write(address, value)
-        when 0xC000..0xDFFF then @wram.write_byte(address, value)
-        when 0xE000..0xFDFF then @wram.write_byte(address, value - 0x2000)
-        when 0xFE00..0xFE9F then @ppu.write_oam(address, value)
-        when 0xFEA0..0xFEFF then 0xFF
-        when 0xFF00..0xFF7F
-          case address
-          when 0xFF00         then @joypad.p1 = value
-          when 0xFF01         then @serial_port.sb = value
-          when 0xFF02         then @serial_port.sc = value
-          when 0xFF04..0xFF07 then @timer.write_registers(address, value)
-          when 0xFF0F         then @interrupts.if = value
-          when 0xFF40..0xFF4B then @ppu.write_registers(address, value)
-          else 0xFF
+        if address.between?(0x0000, 0x7FFF)
+          @cartridge.rom_write(address, value)
+        elsif address <= 0x9FFF
+          @vram.write_byte(address, value)
+        elsif address <= 0xBFFF
+          @cartridge.ram_write(address, value)
+        elsif address <= 0xDFFF
+          @wram.write_byte(address, value)
+        elsif address <= 0xFDFF
+          @wram.write_byte(address - 0x2000, value)
+        elsif address <= 0xFE9F
+          @ppu.write_oam(address, value)
+        elsif address <= 0xFEFF
+          return
+        elsif address <= 0xFF7F
+          if address == 0xFF00
+            @joypad.p1 = value
+          elsif address == 0xFF01
+            @serial_port.sb = value
+          elsif address == 0xFF02
+            @serial_port.sc = value
+          elsif address <= 0xFF07
+            @timer.write_registers(address, value)
+          elsif address == 0xFF0F
+            @interrupts.if = value
           end
-        when 0xFF80..0xFFFE then @hram.write_byte(address, value)
-        when 0xFFFF then @interrupts.ie = value
+        elsif address <= 0xFFFE
+          @hram.write_byte(address, value)
+        elsif address == 0xFFFF
+          @interrupts.ie = value
         else
-          raise StandardError, "Memory out of bounds at: #{format('%04X', address)}"
+          raise StandardError, "#{address} is out of bounds"
         end
       end
     end
